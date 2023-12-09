337863a31d795a843331cc426a358e0cb8542213
Conan <cjohnraniel25@gmail.com>
2023-12-09 16:16:24 +0800

    Arigato

---
+++ src/app/registration/registration.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.page.html',
+  styleUrls: ['./registration.page.scss'],
+})
+export class RegistrationPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
+
+export class RadioButtonStringPage {
+  selectedOption: string | null = null;
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    console.log('Selected Option:', option);
+    // You can perform actions based on the selected option here
+  }
+}
